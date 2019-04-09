0feddc576858a9d42ea52c8bfc36a174949519f4
Davide De Rosa <keeshux@gmail.com>
2019-04-09 14:56:32 +0200

    Add JSON generation script

---
+++ scripts/net.rb
@@ -0,0 +1,101 @@
+require "json"
+require "resolv"
+require "ipaddr"
+
+cwd = File.dirname(__FILE__)
+Dir.chdir(cwd)
+
+###
+
+def read_tls_wrap(strategy, dir, file, from, to)
+    lines = File.foreach(file)
+    key = ""
+    lines.with_index { |line, n|
+        next if n < from or n >= to
+        key << line.strip
+    }
+    key64 = [[key].pack("H*")].pack("m0")
+
+    return {
+        strategy: strategy,
+        key: {
+            dir: dir,
+            data: key64
+        }
+    }
+end
+
+###
+
+servers = File.foreach("../template/servers.csv")
+ca = File.read("../template/ca.crt")
+tls_wrap = read_tls_wrap("auth", 1, "../template/ta.key", 1, 17)
+
+cfg = {
+    ca: ca,
+    wrap: tls_wrap,
+    ep: [
+        "UDP:1194",
+        "TCP:443"
+    ],
+    cipher: "AES-256-CBC",
+    auth: "SHA512",
+    frame: 1,
+    ping: 15,
+    reneg: 0,
+    eku: true,
+    random: true
+}
+
+recommended = {
+    id: "default",
+    name: "Default",
+    comment: "256-bit encryption",
+    cfg: cfg
+}
+presets = [recommended]
+
+defaults = {
+    :username => "user@mail.com",
+    :pool => "us1309",
+    :preset => "default"
+}
+
+###
+
+pools = []
+servers.with_index { |line, n|
+    id, country, area, num, hostname = line.strip.split(",")
+
+    addresses = nil
+    if ARGV.length > 0 && ARGV[0] == "noresolv"
+        addresses = []
+    else
+        addresses = Resolv.getaddresses(hostname)
+    end
+    addresses.map! { |a|
+        IPAddr.new(a).to_i
+    }
+
+    pool = {
+        :id => id,
+        :name => "",
+        :country => country.upcase
+    }
+    pool[:area] = area if !area.empty?
+    pool[:num] = num
+    pool[:hostname] = hostname
+    pool[:addrs] = addresses
+    pools << pool
+}
+
+###
+
+infra = {
+    :pools => pools,
+    :presets => presets,
+    :defaults => defaults
+}
+
+puts infra.to_json
+puts
