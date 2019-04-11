ec7a598a5715d20db468bed6c34b9c216e992af2
Davide De Rosa <keeshux@gmail.com>
2019-04-11 19:20:02 +0200

    Do not download certs (client side)

--- scripts/update-servers.sh
+++ scripts/update-servers.sh
@@ -5,13 +5,9 @@ TPL="template"
 TMP="tmp"
 SERVERS_SRC="$TPL/servers.html"
 SERVERS_DST="$TPL/servers.csv"
-CERTS_SRC="$TPL/certs.zip"
 
 mkdir -p $TPL
 curl -L $URL >$SERVERS_SRC
-curl -L $CERTS_URL >$CERTS_SRC
-rm -rf $TMP
-unzip -q $CERTS_SRC -d $TMP
 
 # id,country,area,num,hostname
 grep -E '<span class="mr-2">.*nordvpn.com</span>' $SERVERS_SRC | sed -E 's/.*<span class="mr-2">([A-z]+)(-)?([A-z]+)?([0-9]+)\.nordvpn\.com<.*$/\1\2\3\4,\1,\3,\4,\1\2\3\4.nordvpn.com/' >$SERVERS_DST
