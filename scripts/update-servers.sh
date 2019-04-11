f21e81e3bf7606cf3959ccf03a59bd286de93bc9
Davide De Rosa <keeshux@gmail.com>
2019-04-11 19:23:52 +0200

    Use standard A-Za-z ranges

--- scripts/update-servers.sh
+++ scripts/update-servers.sh
@@ -10,7 +10,7 @@ mkdir -p $TPL
 curl -L $URL >$SERVERS_SRC
 
 # id,country,area,num,hostname
-grep -E '<span class="mr-2">.*nordvpn.com</span>' $SERVERS_SRC | sed -E 's/.*<span class="mr-2">([A-z]+)(-)?([A-z]+)?([0-9]+)\.nordvpn\.com<.*$/\1\2\3\4,\1,\3,\4,\1\2\3\4.nordvpn.com/' >$SERVERS_DST
+grep -E '<span class="mr-2">.*nordvpn.com</span>' $SERVERS_SRC | sed -E 's/.*<span class="mr-2">([A-Za-z]+)(-)?([A-Za-z]+)?([0-9]+)\.nordvpn\.com<.*$/\1\2\3\4,\1,\3,\4,\1\2\3\4.nordvpn.com/' >$SERVERS_DST
 sed -i"" -E "s/,uk,/,gb,/" $SERVERS_DST
 sort $SERVERS_DST >$SERVERS_DST.tmp
 mv $SERVERS_DST.tmp $SERVERS_DST
